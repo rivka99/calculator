8487aa7d64a1c5b205b8a46500163b44874b916a
rivka99 <rivkafl99@gmail.com>
2022-02-08 12:56:34 -0700

    changed enterfunction, fixed factorial, added decimal button

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { HashRouter, HashRouter as Router, Route, Routes } from 'react-router-do
 
 
 function App() {
-  const digits = [{id:1, digit:'1'},{id:2,digit:'2'},{id:3,digit:'3'},{id:4, digit:'4'},{id:5,digit:'5'},
-  {id:6,digit:'6'},{id:7, digit:'7'},{id:8,digit:'8'},{id:9,digit:'9'},{id:0,digit:'0'},{id:10, digit:'+'},
+  const digits = [{id:0,digit:'0'},{id:1, digit:'1'},{id:2,digit:'2'},{id:3,digit:'3'},{id:4, digit:'4'},{id:5,digit:'5'},
+  {id:6,digit:'6'},{id:7, digit:'7'},{id:8,digit:'8'},{id:9,digit:'9'},{id:100, digit:'.'},{id:10, digit:'+'},
   {id:11, digit:'-'},{id:12, digit:'*'},{id:13, digit:'÷'},{id:14, digit:'^'},{id:15, digit:'!'}]
   const [input, setInput] = useState("")
   return (
--- src/CalcBoard.js
+++ src/CalcBoard.js
@@ -4,15 +4,18 @@ import Button from "./Button"
 export default function CalcBoard(props){
     const [resultGot, setResultGot] = useState(false)
     function backButton(){
-        props.setInput(props.input.substring(0,(props.input.length-1)))
+        props.setInput(String(props.input).substring(0,(props.input.length-1)))
     }
     function fact(num){
         if(num===0){
-            return 0
+            return 1
         }
-        return num + fact(num-1)
+        return num * fact(num-1)
     }
-    function enterButton(){
+    function callEnter(){
+        enterButton(props.input)
+    }
+    function enterButton(equation){
         //replace props.input with a parameter 'equation' and pass props.input as a parameter.
         // then add a function to look at the input (with parantesis included, split function into each basic equation
         //(5*4)+((2-4)*(12/3)) should call this function once for the first value, 3 times for the second value (original 2, plus 
@@ -21,58 +24,60 @@ export default function CalcBoard(props){
         let secondVal
         let result
         let badVals = "+/*^"
-        if(badVals.includes(props.input[0])){
+        if(badVals.includes(equation[0])){
             alert("invalid equation")
             props.setInput("")
         }else{
             //first if here should be factorial, if factorial is at ind 0 and no 'badvals' and no - sign in middle
             //.. then it's a simple factorial submission. 
-            if(props.input.indexOf("!") != -1){
-                let pInd = props.input.indexOf("!")
-                secondVal = props.input.substring(0,pInd)
+            if(equation.indexOf("!") != -1){
+                let pInd = equation.indexOf("!")
+                secondVal = equation.substring(0,pInd)
+                console.log(secondVal)
                 result = fact(parseFloat(secondVal))
-                props.setInput(result)
+                let fixResult = result.toExponential(4);
+                props.setInput(fixResult)
                 setResultGot(true)
-            } else if(props.input.indexOf("+") != -1){
-                let pInd = props.input.indexOf("+")
-                firstVal = props.input.substring(0,pInd)
-                secondVal = props.input.substring(pInd+1,props.input.length)
+            } else if(equation.indexOf("+") != -1){
+                let pInd = equation.indexOf("+")
+                firstVal = equation.substring(0,pInd)
+                secondVal = equation.substring(pInd+1,equation.length)
                 result = parseFloat(firstVal)+parseFloat(secondVal) 
                 props.setInput(result)
                 setResultGot(true)
-            }else if(props.input.indexOf("-") != -1 ){
+            }else if(equation.indexOf("-") != -1 ){
                 let pInd
-                if(props.input.indexOf("-") === 0){
-                    pInd = props.input.indexOf("-",1)
+                if(equation.indexOf("-") === 0){
+                    pInd = equation.indexOf("-",1)
                 }else{
-                    pInd = props.input.indexOf("-")
+                    pInd = equation.indexOf("-")
                 }
                 
-                firstVal = props.input.substring(0,pInd)
-                secondVal = props.input.substring(pInd+1,props.input.length)
+                firstVal = equation.substring(0,pInd)
+                secondVal = equation.substring(pInd+1,equation.length)
                 result = parseFloat(firstVal)-parseFloat(secondVal) 
                 props.setInput(result)
                 setResultGot(true)
-            }else if(props.input.indexOf("*") != -1){
-                let pInd = props.input.indexOf("*")
-                firstVal = props.input.substring(0,pInd)
-                secondVal = props.input.substring(pInd+1,props.input.length)
+            }else if(equation.indexOf("*") != -1){
+                let pInd = equation.indexOf("*")
+                firstVal = equation.substring(0,pInd)
+                secondVal = equation.substring(pInd+1,equation.length)
                 result = parseFloat(firstVal)*parseFloat(secondVal) 
                 props.setInput(result)
                 setResultGot(true)
             }
-            else if(props.input.indexOf("÷") != -1){
-                let pInd = props.input.indexOf("÷")
-                firstVal = props.input.substring(0,pInd)
-                secondVal = props.input.substring(pInd+1,props.input.length)
+            else if(equation.indexOf("÷") != -1){
+                let pInd = equation.indexOf("÷")
+                firstVal = equation.substring(0,pInd)
+                secondVal = equation.substring(pInd+1,equation.length)
                 result = parseFloat(firstVal)/parseFloat(secondVal) 
                 let fixResult = +result.toFixed(4);
                 props.setInput(fixResult)
                 setResultGot(true)
-            }else if(props.input.indexOf("^") != -1){
-                let pInd = props.input.indexOf("^")
-                firstVal = props.input.substring(0,pInd)
-                secondVal = props.input.substring(pInd+1,props.input.length)
+            }else if(equation.indexOf("^") != -1){
+                let pInd = equation.indexOf("^")
+                firstVal = equation.substring(0,pInd)
+                secondVal = equation.substring(pInd+1,equation.length)
                 result = Math.pow(parseFloat(firstVal),parseFloat(secondVal))
                 let fixResult = +result.toFixed(4);
                 props.setInput(fixResult)
@@ -88,7 +93,7 @@ export default function CalcBoard(props){
                 props.digits.map(button=><Button key = {button.id} digit = {button.digit} input={props.input} setInput={props.setInput} resultGot={resultGot} setResultGot={setResultGot}/>)
             }   
              <button onClick={backButton} className="dig-btn">⌫</button>
-             <button onClick={enterButton} className="dig-btn">↩</button>
+             <button onClick={callEnter} className="dig-btn">↩</button>
             </div>
            
         </div>
